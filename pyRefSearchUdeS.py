859d719558b9b2df768f49e283ce93c2f69a024f
Paul Charette <Paul.Charette@USherbrooke.ca>
2025-02-24 21:24:30 -0500

    V1.1

--- pyRefSearchUdeS.py
+++ pyRefSearchUdeS.py
@@ -528,7 +528,8 @@ def _add_coauthor_columns_and_clean_up_publications_df(
         for co_authors in publications["Auteurs locaux"]
     ]
 
-    # Check that at least one local author, else flag publication
+    # Check that at least one local author in the list of author Scopus IDs.
+    # If not: the only local author probably has more than one Scopus ID, show warning
     for _, row in publications.iterrows():
         if not row["Auteurs locaux"]:
             print(
