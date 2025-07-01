9534d176d7def5d6a52e20605f5c42a268a10496
Paul Charette <Paul.Charette@USherbrooke.ca>
2025-07-01 16:29:40 -0400

    V1.4

---
+++ pyrefsearch_diff - REMOTE.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: Run the Scopus search
+echo Running pyrefsearch.py...
+set PYTHONDIR="C:\Users\chap1202\AppData\Local\Programs\Python\Python312"
+set WORKINGDIR="C:\Users\chap1202\OneDrive - USherbrooke\Documents on OneDrive\Python\Pycharm\pyrefsearch"
+cd %WORKINGDIR%
+%PYTHONDIR%\python.exe pyrefsearch\pyrefsearch.py data\pyrefsearch_diff.toml > pyrefsearch.log 2>&1
+
+:: if the PowerShell script "pyrefsearch_send_email_confirmation.ps1" exists, pyrefsearch.py ran successfully
+set EMAIL_POWERSHELL_SCRIPT="pyrefsearch_send_email_confirmation.ps1"
+if exist %EMAIL_POWERSHELL_SCRIPT% GOTO run_power_shell_script
+GOTO pyrefsearch_failed
+
+:: pydersearch.py ran correctly, send confirmation emails, delete the PowerShell script
+:run_power_shell_script
+echo Sending email confirmations...
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File %EMAIL_POWERSHELL_SCRIPT%
+del %EMAIL_POWERSHELL_SCRIPT%
+GOTO end
+
+:: pyrefsearch.py failed to run, send email tp Paul.Charette@USherbrooke.ca with logfile
+:pyrefsearch_failed
+echo pyrefsearch.py failed to run, send error log email...
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File send_email_pyrefsearch_failure_to_run.ps1
+GOTO end
+
+:end
+pause
