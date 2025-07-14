f193b377380301b42980f040d56c3ac376d4326c
Paul Charette <Paul.Charette@USherbrooke.ca>
2025-07-14 14:48:00 -0400

    V1.4

--- pyrefsearch_diff.bat
+++ pyrefsearch_diff.bat
@@ -23,7 +23,7 @@ set EMAIL_POWERSHELL_SCRIPT="shell_scripts\pyrefsearch_send_email_confirmation.p
 if exist  %EMAIL_POWERSHELL_SCRIPT% del /F  %EMAIL_POWERSHELL_SCRIPT%
 %PYTHONDIR%\python.exe pyrefsearch\pyrefsearch.py data\pyrefsearch_diff.toml > pyrefsearch.log 2>&1
 
-:: if the PowerShell script "pyrefsearch_send_email_confirmation.ps1" exists, pyrefsearch.py ran successfully
+:: if the PowerShell script %EMAIL_POWERSHELL_SCRIPT% exists, pyrefsearch.py ran successfully
 if exist %EMAIL_POWERSHELL_SCRIPT% GOTO pyrefsearch_success
 GOTO pyrefsearch_failed
 
@@ -35,7 +35,7 @@ del %EMAIL_POWERSHELL_SCRIPT%
 
 GOTO end
 
-:: pyrefsearch.py failed to run, send email tp Paul.Charette@USherbrooke.ca with logfile
+:: pyrefsearch.py failed to run, send email to Paul.Charette@USherbrooke.ca with logfile
 :pyrefsearch_failed
 echo pyrefsearch.py failed to run, send error log email...
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File shell_scripts\send_email_pyrefsearch_failure_to_run.ps1
