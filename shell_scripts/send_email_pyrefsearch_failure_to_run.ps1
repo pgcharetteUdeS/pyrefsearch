4d18a0b508098e0edd6bab9c6a82c7a8bc12891c
Paul Charette <Paul.Charette@USherbrooke.ca>
2025-07-14 14:33:21 -0400

    V1.4

--- shell_scripts/send_email_pyrefsearch_failure_to_run.ps1
+++ shell_scripts/send_email_pyrefsearch_failure_to_run.ps1
@@ -1,6 +1,6 @@
 # Script to send a failure email and logfile to Paul.Charette@USherbrooke.ca
 $Subject = "pydefsearch.py failed to run! If running from home, need VPN..."
-$Body = "pydefsearch.py failed to run! If running from home, need VPN..."
 $currentDirectory = (Get-Location).Path
 $logfilename = $currentDirectory + "\pyrefsearch.log"
-& ".\shell_scripts\send_email.ps1" -EmailTo "Paul.Charette@USherbrooke.ca" -Subject $Subject -Body $Body -AttachmentFilename $logfilename
+$attachments = @($logfilename)
+& ".\shell_scripts\send_email.ps1" -EmailTo "Paul.Charette@USherbrooke.ca" -Subject $Subject -Body $Subject -Attachments $attachments
