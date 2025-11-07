5cf38fef99e57f2772d1a006b5e2deb15057c9d2
Paul Charette <paul.charette@usherbrooke.ca>
2025-11-07 15:10:13 -0500

    V2.0

--- pyrefsearch.bat
+++ pyrefsearch.bat
@@ -5,5 +5,5 @@ copy "C:\Users\%USERNAME%\USherbrooke\3IT - Gestion Centrale - Documents\General
 echo Running pyrefsearch.py...
 %PYTHONDIR%\python.exe pyrefsearch\pyrefsearch.py data\pyrefsearch.toml > pyrefsearch.log 2>&1
 %PYTHONDIR%\python.exe pyrefsearch\ansi_to_html_converter.py pyrefsearch.log pyrefsearch.html
-%PYTHONDIR%\python.exe pyrefsearch\strip_ANSI_codes_from_file.py pyrefsearch.log pyrefsearch.txt
+%PYTHONDIR%\python.exe pyrefsearch\strip_ANSI_codes_from_file.py pyrefsearch.log pyrefsearch.log
 pause
